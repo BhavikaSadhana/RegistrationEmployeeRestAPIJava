1f42272b1ac55d27771a7245cec8e73ee767ed6d
BhavikaSadhana <bhavikasadhana@gmail.com>
2025-03-10 19:52:56 +0530

    Update ResourceNotFoundException.java

--- ResourceNotFoundException.java
+++ ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.bhavana.springboot.exception;
+package com.bhavana.springboot;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
