78c2e7a3627e6e3dff2a32fb14a5ea6840f7902b
BhavikaSadhana <bhavikasadhana@gmail.com>
2025-03-10 19:59:32 +0530

    Update EmployeeRepository.java

--- java/com/bhavana/springboot/EmployeeRepository.java
+++ java/com/bhavana/springboot/EmployeeRepository.java
@@ -1,9 +1,9 @@
-package com.bhavana.springboot.repository;
+package com.bhavana.springboot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bhavana.springboot.model.Employee;
+import com.bhavana.springboot.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long>{
 
